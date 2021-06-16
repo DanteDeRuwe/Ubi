46f0f73be8dbcec91662370735746c5176384f07
Dante De Ruwe <dantederuwe@gmail.com>
2021-06-16 14:17:45 +0200

    Clean up template

--- Ubi.Api/Program.cs
+++ Ubi.Api/Program.cs
@@ -1,23 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Ubi.Api;
 
-namespace Ubi.Api
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
-}
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>())
+    .Build()
+    .Run();
