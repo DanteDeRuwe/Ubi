6432c8ac464fb86dbc840fb421fae80ab66ee615
Dante De Ruwe <dantederuwe@gmail.com>
2021-06-17 11:20:40 +0200

    Add tests for valueobjects

---
+++ Ubi.UnitTests/LatitudeTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FluentAssertions;
+using Ubi.Core.ValueObjects;
+using Xunit;
+
+namespace Ubi.Tests
+{
+    public class LatitudeTest
+    {
+        [Theory]
+        [InlineData(-90)]
+        [InlineData(90)]
+        [InlineData(0)]
+        [InlineData(1.23456)]
+        public void Latitude_ShouldBeSuccessful_WhenGivenGoodValue(double value)
+        {
+            //Arrange + Act
+            Func<Latitude> act = () => Latitude.From(value);
+            
+            //Assert
+            act.Should().NotThrow();
+            act.Invoke().Value.Should().Be(value);
+        }
+        
+        [Theory]
+        [InlineData(-90.001)]
+        [InlineData(90.00001)]
+        [InlineData(-int.MaxValue)]
+        public void Latitude_ShouldThrow_WhenGivenBadValue(double value)
+        {
+            //Arrange + Act
+            Func<Latitude> act = () => Latitude.From(value);
+            
+            //Assert
+            act.Should().Throw<ArgumentException>().WithMessage("The Latitude is incorrectly set*");
+        }
+    }
+}
---
+++ Ubi.UnitTests/LongitudeTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using Ubi.Core.ValueObjects;
+using Xunit;
+
+namespace Ubi.Tests
+{
+    public class LongitudeTest
+    {
+        [Theory]
+        [InlineData(-90)]
+        [InlineData(90)]
+        [InlineData(180)]
+        [InlineData(-180)]
+        [InlineData(0)]
+        [InlineData(1.23456)]
+        public void Longitude_ShouldBeSuccessful_WhenGivenGoodValue(double value)
+        {
+            //Arrange + Act
+            Func<Longitude> act = () => Longitude.From(value);
+            
+            //Assert
+            act.Should().NotThrow();
+            act.Invoke().Value.Should().Be(value);
+        }
+        
+        [Theory]
+        [InlineData(-180.001)]
+        [InlineData(180.00001)]
+        [InlineData(-int.MaxValue)]
+        public void Longitude_ShouldThrow_WhenGivenBadValue(double value)
+        {
+            //Arrange + Act
+            Func<Longitude> act = () => Longitude.From(value);
+            
+            //Assert
+            act.Should().Throw<ArgumentException>().WithMessage("The Longitude is incorrectly set*");
+        }
+    }
+}
