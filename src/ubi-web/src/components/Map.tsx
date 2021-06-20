f4ad2b419a71e536ac9cb5b2c86cb7ee040deb30
Dante De Ruwe <dantederuwe@gmail.com>
2021-06-20 21:50:32 +0200

    Add map logic with markers and popups

--- src/ubi-web/src/components/Map.tsx
+++ src/ubi-web/src/components/Map.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import mapboxgl, { Map as MapBoxMap } from 'mapbox-gl';
+import mapboxgl, { Map as MapBoxMap, Marker, Popup } from 'mapbox-gl';
 import { Camera } from '../types';
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFudGVkZXJ1d2UiLCJhIjoiY2txNWNsM3ZhMTN4aTJ2a2FodTk1N290MyJ9.FKu3YbJpcy_F9ZOt69VOcg';
 
@@ -7,21 +7,34 @@ interface MapProps {
   cameras: Camera[];
 }
 
+const lat = 52.0914;
+const long = 5.1115;
+const zoom = 12;
+
 const Map: React.FC<MapProps> = ({ cameras }) => {
   const mapContainer = React.useRef() as React.MutableRefObject<HTMLDivElement>;
   const map = React.useRef<MapBoxMap>();
-  const [lat, setLat] = React.useState(52.0914);
-  const [long, setLong] = React.useState(5.1115);
-  const [zoom, setZoom] = React.useState(12);
 
   React.useEffect(() => {
-    if (map.current) return; // initialize map only once
+    if (cameras.length < 1 || map.current) return;
     map.current = new MapBoxMap({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [long, lat],
       zoom: zoom,
     });
+
+    cameras.forEach(camera =>
+      new Marker()
+        .setLngLat([camera.longitude.value, camera.latitude.value])
+        .setPopup(
+          new Popup({ offset: 25 }) // add popups
+            .setHTML(
+              `<h3>${camera.fullName}</h3><small>(ID: ${camera.number}, LAT: ${camera.latitude.value}, LON: ${camera.longitude.value})</small>`
+            )
+        )
+        .addTo(map.current as MapBoxMap)
+    );
   });
 
   return (
