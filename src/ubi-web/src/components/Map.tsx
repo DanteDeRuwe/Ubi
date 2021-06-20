7fe8f967c634a79fa4a3789ff062ee7fabbd55a9
Dante De Ruwe <dantederuwe@gmail.com>
2021-06-20 17:57:41 +0200

    Add MapBox

--- src/ubi-web/src/components/Map.tsx
+++ src/ubi-web/src/components/Map.tsx
@@ -1,5 +1,29 @@
 import * as React from 'react';
+import mapboxgl, { Map as MapBoxMap } from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGFudGVkZXJ1d2UiLCJhIjoiY2txNWNsM3ZhMTN4aTJ2a2FodTk1N290MyJ9.FKu3YbJpcy_F9ZOt69VOcg';
 
-const Map: React.FC = () => <div id="map"></div>;
+const Map: React.FC = () => {
+  const mapContainer = React.useRef() as React.MutableRefObject<HTMLDivElement>;
+  const map = React.useRef<MapBoxMap>();
+  const [lat, setLat] = React.useState(52.0914);
+  const [long, setLong] = React.useState(5.1115);
+  const [zoom, setZoom] = React.useState(12);
+
+  React.useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new MapBoxMap({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [long, lat],
+      zoom: zoom,
+    });
+  });
+
+  return (
+    <div id="map">
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
+};
 
 export default Map;
